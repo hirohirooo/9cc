5a2ccc8f7b29e737154771d96b57ff6d95aa2a86
Hiroshi Nakano <hiroshi_nakano@nakano.Ubuntu>
2023-04-09 15:34:43 +0900

    再帰下降構文解析

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -5,7 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef enum {
+typedef enum
+{
   TK_RESERVED, // Keywords or punctuators
   TK_NUM,      // Integer literals
   TK_EOF,      // End-of-file markers
@@ -13,12 +14,95 @@ typedef enum {
 
 // Token type
 typedef struct Token Token;
-struct Token {
+
+// Tokenの型
+struct Token
+{
   TokenKind kind; // Token kind
   Token *next;    // Next token
   int val;        // If kind is TK_NUM, its value
   char *str;      // Token string
 };
+
+// 抽象構文木のノードの種類
+typedef enum
+{
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM  // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+// 抽象構文木ノードの型
+struct Node
+{
+  NodeKind kind; // ノードの型
+  Node *lhs;     // 左辺
+  Node *rhs;     // 右辺
+  int val;       // kindがND_NUMの場合のみ使う
+};
+
+// 新しいノードを作成する関数
+// 2項演算子用
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+// 数値用
+Node *new_node_num(int val)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr()
+{
+  Node *node = mul();
+
+  for (;;)
+  {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul(){
+  Node *node = primary();
+
+  for(;;){
+    if(consume('*'))
+      node = new_code(ND_MUL,node,primary());
+    else if(consume('/'))
+      node = new_code(ND_DIV,node,primary());
+    else
+      return node;
+  }
+}
+
+Node *primary() {
+  // 次のトークンが"("なら、"(" expr ")"のはず
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  // そうでなければ数値のはず
+  return new_node_num(expect_number());
+}
 
 // Input program
 char *user_input;
@@ -27,7 +111,8 @@ char *user_input;
 Token *token;
 
 // Reports an error and exit.
-void error(char *fmt, ...) {
+void error(char *fmt, ...)
+{
   va_list ap;
   va_start(ap, fmt);
   vfprintf(stderr, fmt, ap);
@@ -36,7 +121,8 @@ void error(char *fmt, ...) {
 }
 
 // Reports an error location and exit.
-void error_at(char *loc, char *fmt, ...) {
+void error_at(char *loc, char *fmt, ...)
+{
   va_list ap;
   va_start(ap, fmt);
 
@@ -50,7 +136,8 @@ void error_at(char *loc, char *fmt, ...) {
 }
 
 // Consumes the current token if it matches `op`.
-bool consume(char op) {
+bool consume(char op)
+{
   if (token->kind != TK_RESERVED || token->str[0] != op)
     return false;
   token = token->next;
@@ -58,14 +145,16 @@ bool consume(char op) {
 }
 
 // Ensure that the current token is `op`.
-void expect(char op) {
+void expect(char op)
+{
   if (token->kind != TK_RESERVED || token->str[0] != op)
     error_at(token->str, "expected '%c'", op);
   token = token->next;
 }
 
 // Ensure that the current token is TK_NUM.
-int expect_number() {
+int expect_number()
+{
   if (token->kind != TK_NUM)
     error_at(token->str, "expected a number");
   int val = token->val;
@@ -73,12 +162,14 @@ int expect_number() {
   return val;
 }
 
-bool at_eof() {
+bool at_eof()
+{
   return token->kind == TK_EOF;
 }
 
 // Create a new token and add it as the next token of `cur`.
-Token *new_token(TokenKind kind, Token *cur, char *str) {
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
   Token *tok = calloc(1, sizeof(Token));
   tok->kind = kind;
   tok->str = str;
@@ -87,27 +178,32 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 }
 
 // Tokenize `user_input` and returns new tokens.
-Token *tokenize() {
+Token *tokenize()
+{
   char *p = user_input;
   Token head;
   head.next = NULL;
   Token *cur = &head;
 
-  while (*p) {
+  while (*p)
+  {
     // Skip whitespace characters.
-    if (isspace(*p)) {
+    if (isspace(*p))
+    {
       p++;
       continue;
     }
 
     // Punctuator
-    if (*p == '+' || *p == '-') {
+    if (*p == '+' || *p == '-')
+    {
       cur = new_token(TK_RESERVED, cur, p++);
       continue;
     }
 
     // Integer literal
-    if (isdigit(*p)) {
+    if (isdigit(*p))
+    {
       cur = new_token(TK_NUM, cur, p);
       cur->val = strtol(p, &p, 10);
       continue;
@@ -120,8 +216,10 @@ Token *tokenize() {
   return head.next;
 }
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
+int main(int argc, char **argv)
+{
+  if (argc != 2)
+  {
     error("%s: invalid number of arguments", argv[0]);
     return 1;
   }
@@ -137,8 +235,10 @@ int main(int argc, char **argv) {
   printf("  mov rax, %d\n", expect_number());
 
   // ... followed by either `+ <number>` or `- <number>`.
-  while (!at_eof()) {
-    if (consume('+')) {
+  while (!at_eof())
+  {
+    if (consume('+'))
+    {
       printf("  add rax, %d\n", expect_number());
       continue;
     }
